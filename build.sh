9c85a0243f8d50881330cfbbca933e2c12971a63
lenbo <islenbo@qq.com>
2017-09-21 19:23:42 +0800

    refactor build.sh
    auto pack for mac and linux

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-go build -o autossh main.go
+VERSION="v0.1"
+PROJECT="autossh"
+
+function build() {
+    os=$1
+    arch=$2
+    package="${PROJECT}-${os}-${arch}_${VERSION}"
+
+    echo "build ${package} ..."
+    mkdir -p "./releases/${package}"
+    CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -o "./releases/${package}/autossh" main.go
+    cp ./servers.example.json "./releases/${package}/servers.json"
+    cd ./releases/
+    zip -r "./${package}.zip" "./${package}"
+    echo "clean ${package}"
+    rm -rf "./${package}"
+    cd ../
+}
+
+# Linux
+build linux amd64
+build linux 386
+build linux arm
+
+# OS X Mac
+build darwin amd64
